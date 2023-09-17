b9e8a361e4dcaca0a954f1ac313bc9eeb1f10fb0
Inna949Festchuk <innabori@mail.ru>
2023-09-17 06:57:22 +0200

    -

--- analyze2.py
+++ analyze2.py
@@ -2,10 +2,10 @@
 import osgeo.ogr
 
 shapefile = osgeo.ogr.Open('Data/Task1/tl_2014_us_state.shp')
-number_feature = int(input('Введите номер геообъекта: '))
+
 layer =shapefile.GetLayer(0)
-feature = layer.GetFeature(number_feature)
-print(f'Геообъект № {number_feature} имеет следующие атрибуты:')
+feature = layer.GetFeature(12)
+print(f'Геообъект № {12} имеет следующие атрибуты:')
 print()
 
 attributes = feature.items()
